b8dc28e3decea3bc32745eef0d09627938088064
JavaByMohamed <mohamed_abdel.monem@hotmail.se>
2021-05-07 16:31:58 +0200

    Mo

--- src/App.js
+++ src/App.js
@@ -1,58 +1,109 @@
-import logo from './slack.svg';
+import logo from './visual-studio.svg';
 import './App.css';
+import {useState} from "react";
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <div className='App-Background'>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App-Form">
-          <b>Welcome!</b>
-
-          <div className='container-usertype'>
-            <button className='btn-usertype'>Freelancer</button> &nbsp;
-            <button className='btn-usertype'>Corperate</button>
-          </div>
-
-          <div className='container-btn-bankId'> <button className='btn-bankId'> Sign in with bankID</button></div>
-          <p className='orDivider'> **** OR **** </p>
-
-          <form className='Register_form'>
-
-            <input className='inputValue' type="email" required placeholder="Email" /><br />
+  const [changeForm, setChangeForm] = useState('false');
 
 
-            <input type="password" id="pwd" name="pwd" required minLength="6" placeholder="Password"></input><br></br>
 
-            <label for="taxation">Your taxation country</label><br />
-            <select name="taxation" id="cars">
-              <option value="Sweden">Sweden</option>
-              <option value="England">England</option>
-              <option value="Finland">Finland</option>
-              <option value="Norway">Norway</option>
-            </select>
-            <br></br>
+  function form1() {
+    if(changeForm === true ){
 
-            <label for="language">Language</label><br />
-            <select name="language" id="cars">
-              <option value="Swedish">Swedish</option>
-              <option value="English">English</option>
-            </select>
-            <div className='checkbox'>
-              <input type="checkbox" className="checkbox" value="check" id="agree" required />
-            We need your consist to be able to provide<br></br>you with information based on your profile.<br />Read more in our <a href="url">Privacy Policy</a><br />
-            </div>
-            <button type="submit" className='SubmitBtn'><b>LOGGA IN</b></button>
-          </form>
-        </div>
-        <div className='OutsideBox'>As a user of the INEXT Freelancer service I hereby declares that I am<br />over 18 years old and approve the <b>Temporary Worker Agreement<br />and General Terms and Conditions.<b /></b></div>
-     
-      <div className='OutsideLogin' >Har du ett konto? <a href="url">Logga in</a></div>
-      </div>
-      
+    return(
+    <form className='Register_form'>
+
+    <input className='inputValue' type="email" required placeholder="Email" /><br />
+
+
+    <input type="password" id="pwd" name="pwd" required minLength="6" placeholder="Password"></input><br></br>
+
+    <label for="taxation">Your taxation country</label><br />
+    <select name="taxation" id="cars">
+      <option value="Sweden">Sweden</option>
+      <option value="England">England</option>
+      <option value="Finland">Finland</option>
+      <option value="Norway">Norway</option>
+    </select>
+    <br></br>
+
+    <label for="language">Language</label><br />
+    <select name="language" id="language">
+      <option value="Swedish">Swedish</option>
+      <option value="English">English</option>
+    </select>
+    <div className='checkbox'>
+      <input type="checkbox" className="checkbox" value="check" id="agree" required />
+    We need your consist to be able to provide<br></br>you with information based on your profile.<br />Read more in our <a href="url">Privacy Policy</a><br />
     </div>
+    <button type="submit" className='SubmitBtn'><b>Sign up</b></button>
+  </form>
+  )
+}else {
+return (
+  <form className='Register_form'>
+
+  <input className='inputValue' type="email" required placeholder="Email" /><br />
+
+
+  <input type="password" id="pwd" name="pwd" required minLength="6" placeholder="Password"></input><br></br>
+
+  <label for="taxation">Where is the company?</label><br />
+  <select name="taxation" id="cars">
+    <option value="Sweden">Sweden</option>
+    <option value="England">England</option>
+    <option value="Finland">Finland</option>
+    <option value="Norway">Norway</option>
+    <option value="Other">Other</option>
+  </select>
+  <br></br>
+
+  <label for="company-focus">Company focus?</label><br />
+  <select name="company-focus" id="cf">
+    <option value="Finance/Economy"> Finance/Economy </option>
+    <option value="Real estate"> Real estate</option>
+    <option value="Technology"> Technology</option>
+    <option value="Architecture"> Architecture</option>
+    <option value="Other"> Other</option>
+
+  </select>
+  <div className='checkbox'>
+    <input type="checkbox" className="checkbox" value="check" id="agree" required />
+  We need your consist to be able to provide<br></br>you with information based on your profile.<br />Read more in our <a href="url">Privacy Policy</a><br />
+  </div>
+  <button type="submit" className='SubmitBtn'><b>Sign up</b></button>
+</form>
+)
+}
+
+  }
+  return (
+   
+    <div className="App">
+    <div className='App-Background'>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-Form">
+        <b>Welcome!</b>
+
+        <div className='container-usertype'>
+          <button className='btn-usertype' onClick={() => setChangeForm(true)}>Freelancer</button> &nbsp;
+          <button className='btn-usertype' onClick={() => setChangeForm(false)}>Corperate</button>
+        </div>
+       
+
+        <div className='container-btn-bankId'> <button className='btn-bankId'> Sign in with bankID</button></div>
+        <p className='orDivider'> **** OR **** </p>
+        {form1()}
+
+      </div>
+      <div className='OutsideBox'>As a user of the INEXT Freelancer service I hereby declares that I am<br />over 18 years old and approve the <b>Temporary Worker Agreement<br />and General Terms and Conditions.<b /></b></div>
+    <div className='OutsideLogin' >Do you have an account? <a href="url">Sign in</a></div>
+    
+    </div>
+  </div>
+
   );
 }
 
